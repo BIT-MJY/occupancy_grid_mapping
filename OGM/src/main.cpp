92bafda68a45489efffa9c48c7f96309fa9f09b0
Junyi Ma <56945855+BIT-MJY@users.noreply.github.com>
2021-09-08 11:03:16 +0800

    Update main.cpp

--- OGM/src/main.cpp
+++ OGM/src/main.cpp
@@ -8,10 +8,10 @@
 
 void getParamFromYaml(int &map_size_x, int &map_size_y, double &map_resolution, bool &calib)
 {
-    ros::param::get("~map_size_x_",map_size_x);  // x方向栅格数目
-    ros::param::get("~map_size_y_",map_size_y);  // y方向栅格数目
-    ros::param::get("~resolution_",map_resolution);  // m/格子
-    ros::param::get("~calib",calib);  // 是否进行激光雷达数据矫正
+    ros::param::get("~map_size_x_",map_size_x); 
+    ros::param::get("~map_size_y_",map_size_y); 
+    ros::param::get("~resolution_",map_resolution);  
+    ros::param::get("~calib",calib); 
 }
 
 
@@ -23,8 +23,6 @@ int main (int argc, char** argv) {
     bool calib;
     getParamFromYaml(map_size_x, map_size_y, map_resolution, calib);
     tf::TransformListener tf(ros::Duration(10.0));
-    // 300*300的栅格，分辨率为0.1m/格
-    // MapBuilder map_builder(300, 300, 0.1, &tf);
     MapBuilder map_builder(map_size_x, map_size_y, map_resolution, &tf, calib);
     ros::spin();
 }
